7ee747ed6e580c2c21126045dacf2748b156a563
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2024-04-05 11:45:14 -0600

    Files for plane finding

---
+++ catkin_ws/src/vision/geometric_features/scripts/Utils.py
@@ -0,0 +1,13 @@
+import numpy
+import ros_numpy
+import cv2
+
+def point_cloud_2_to_cv_images(msg):
+    arr = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
+    rgb_arr = arr['rgb'].copy()
+    rgb_arr.dtype = numpy.uint32
+    r = numpy.asarray(((rgb_arr >> 16) & 255), dtype='uint8')
+    g = numpy.asarray(((rgb_arr >>  8) & 255), dtype='uint8')
+    b = numpy.asarray(((rgb_arr      ) & 255), dtype='uint8')
+    img_bgr = cv2.merge((b,g,r))
+    return img_bgr, arr 
